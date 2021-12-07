6f6988841f6cbece0300f9d94f8d6caf86d80473
Artem Safin <artemsafin.webdev@gmail.com>
2021-12-07 15:17:13 +0300

    Адаптив главной

--- src/js/latestNews.js
+++ src/js/latestNews.js
@@ -10,17 +10,21 @@ export default function latestNews() {
 
         new Swiper(container, {
             watchOverflow: true,
-            spaceBetween: 20,
-            slidesPerView: 2,
+            spaceBetween: 10,
+            slidesPerView: 'auto',
             navigation: {
                 nextEl: element.querySelector('.latest-news__slider-arrow--next'),
                 prevEl: element.querySelector('.latest-news__slider-arrow--prev')
             },
             breakpoints: {
+                641: {
+                    slidesPerView: 2,
+                    spaceBetween: 20
+                },
                 1024: {
                     slidesPerView: 3,
+                    spaceBetween: 20
                 }
-
             }
         });
     });
